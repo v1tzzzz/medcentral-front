a2f965b8aaac6ac7ca2aca40587d9cd5fa81d155
v1tzzzz <aly.vtzcode@gmail.com>
2025-11-14 10:08:35 -0300

    try: csp on firefox to vlibras work

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,22 @@ export default defineConfig({
   server: {
     headers: {
       // Habilita WebAssembly
+      'content-security-policy':
+        "default-src 'self'; " +
+        "script-src 'self' 'unsafe-inline' 'unsafe-eval' 'wasm-unsafe-eval' https://vlibras.gov.br; " +
+        "style-src 'self' 'unsafe-inline' https://vlibras.gov.br; " +
+        "img-src 'self' data: https: https://vlibras.gov.br; " +
+        "font-src 'self' data: https://vlibras.gov.br; " +
+        "connect-src 'self' https://vlibras.gov.br wss://localhost:*; " +
+        "frame-src https://vlibras.gov.br; " +
+        "worker-src 'self' blob:; " +
+        "child-src 'self' blob:;"
+    }
+  },
+
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true
     }
   }
 })
